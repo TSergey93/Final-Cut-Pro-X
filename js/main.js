eb0076c4b0ae31e98dff4202969059af6067c962
TSergey93 <Mitegnik1@gmail.com>
2017-10-07 16:40:58 +0300

    Переделано меню под устройства с небольшим разрешением экрана

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var picture_1 = document.getElementById("picture_1");
 var picture_2 = document.getElementById("picture_2");
+var mobile_menu = document.getElementById("mobile_menu_arrow");
+var nav = document.querySelector("header nav");
+var modal_overlay = document.querySelector(".modal_overlay");
 
 picture_1.addEventListener("click", function(event) {
     event.preventDefault();
@@ -9,4 +12,10 @@ picture_1.addEventListener("click", function(event) {
 picture_2.addEventListener("click", function(event) {
     event.preventDefault();
     picture_1.classList.toggle("display");
+});
+
+mobile_menu.addEventListener("click", function(event) {
+    mobile_menu.classList.toggle("mobile_menu_arrow_click");
+    nav.classList.toggle("nav_mobile_click");
+    modal_overlay.classList.toggle("display");
 });
