49cb917cfd1415cc6c4599856d2ed5af8ca90750
TSergey93 <Mitegnik1@gmail.com>
2017-10-09 00:19:11 +0300

    Небольшой фикс меню под мобильные устройства

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var mobile_menu = document.getElementById("mobile_menu_arrow");
 var nav = document.querySelector("header nav");
 var modal_overlay = document.querySelector(".modal_overlay");
 
+/*picture_1 и picture_2 отвечают за скрытие картинок*/
+
 picture_1.addEventListener("click", function(event) {
     event.preventDefault();
     picture_1.classList.toggle("display");
@@ -14,7 +16,17 @@ picture_2.addEventListener("click", function(event) {
     picture_1.classList.toggle("display");
 });
 
+/*Функция отвечает за появление меню для мобильных устройств*/
+
 mobile_menu.addEventListener("click", function(event) {
+    mobile_menu.classList.toggle("mobile_menu_arrow_click");
+    nav.classList.toggle("nav_mobile_click");
+    modal_overlay.classList.toggle("display");
+});
+
+/*Функция отвечает за закрытие меню для мобильных устройств*/
+
+modal_overlay.addEventListener("click", function(event) {
     mobile_menu.classList.toggle("mobile_menu_arrow_click");
     nav.classList.toggle("nav_mobile_click");
     modal_overlay.classList.toggle("display");
